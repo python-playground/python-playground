b61157e7edc5cb9aac1ecb0d6da5b012cd89c3f7
Yeuk Hon Wong <yeukhon@acm.org>
2014-08-30 22:14:32 -0400

    Created a scaffold of the regular expression directory.

---
+++ regular_expressions/tests/test_scanner.py
@@ -0,0 +1,11 @@
+import unittest
+
+from .. import scanner
+
+class TestScanner(unittest.TestCase):
+    """Tests scanner.py."""
+    def test_scanner_passed(self):
+        pass        
+
+if __name__ == "__main__":
+    unittest.main()
