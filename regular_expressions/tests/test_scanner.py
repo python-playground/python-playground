ee578d9198bc99c86553566393bbf81116cc4519
Yeuk Hon Wong <yeukhon@acm.org>
2014-08-30 22:19:20 -0400

    Written some scanner test code. TDD okay?

--- regular_expressions/tests/test_scanner.py
+++ regular_expressions/tests/test_scanner.py
@@ -5,7 +5,13 @@ from .. import scanner
 class TestScanner(unittest.TestCase):
     """Tests scanner.py."""
     def test_scanner_passed(self):
-        pass        
+        password = "He1l0"
+        tokens = scanner.get_tokens(password)
+        self.assertEqual(tokens,[password])
+
+    def test_scanner_raised_exception_on_bad_input(self):
+        password = "Hello World"
+        self.assertRaises(Exception, scanner.get_tokens, (password,))
 
 if __name__ == "__main__":
     unittest.main()
